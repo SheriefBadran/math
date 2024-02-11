0667b5ef822058ddff69d73b71cbf4b363c674aa
Sherief Badran <sherief@beanloop.se>
2024-02-11 22:53:46 +0000

    Added option for angle mode radians or degrees as input parameter to function

--- src/linear-algebra/vectors/dot-product.py
+++ src/linear-algebra/vectors/dot-product.py
@@ -3,24 +3,25 @@ import math
 
 # Consider the vectors u = (2, -1, 1) and v = (1, 1, 2). Find u • v and determine the angel betwen u and v
 
-def calculate_vectors_angle(u: np.ndarray, v: np.ndarray) -> float :
+def calculate_vectors_angle(u: np.ndarray, v: np.ndarray, in_radians: bool = True) -> float :
 
     if not isinstance(u, np.ndarray) or not isinstance(v, np.ndarray):
-        raise TypeError('Expected arr to be an np.ndarray')
+        raise TypeError('Expected array to be an np.ndarray')
+    
+    if not isinstance(in_radians, bool):
+        raise TypeError('Expected in_radians to be a boolean')
     
     dot_uv = u.dot(v)
     norm_u = np.linalg.norm(u)
     norm_v = np.linalg.norm(v)
     
     cos_theta = dot_uv/(norm_u * norm_v)
-    return np.arccos(cos_theta)
+    theta = np.arccos(cos_theta)
+    return theta if in_radians else math.degrees(theta)
 
-rad_angle_uv = calculate_vectors_angle(np.array([2, -1, 1]), np.array([1, 1, 2]))
+rad_angle_uv = calculate_vectors_angle(np.array([2, -1, 1]), np.array([1, 1, 2]), in_radians=True)
 
-# Since arccos returns the angle theta in radians, we rather express the answer in terms of pi
-theta_in_pi = rad_angle_uv/np.pi
-
-print("angle between u and v: ", theta_in_pi)
+print("Angle between u and v: ", rad_angle_uv)
 
 
 
