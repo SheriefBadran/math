0b94f49e06e56955bf151c95501e4fbff1de9683
Sherief Badran <sherief@beanloop.se>
2024-03-07 19:17:24 +0000

    Added Lagrange's identity proof

--- src/linear-algebra/vectors/cross-product-properties.py
+++ src/linear-algebra/vectors/cross-product-properties.py
@@ -12,17 +12,17 @@ v = sp.Matrix([d, e, f])
 print('u • (u x v) = 0: ', sp.simplify(u.dot(u.cross(v))) == 0)
 print('v • (u x v) = 0: ', sp.simplify(v.dot(u.cross(v))) == 0)
 
-# Prove the theorem u x (v x w) = (u • w)v - (u • v)w
+# Prove the theorem u x (v x w) = (u • w)v - (u • v)w (Relationship between cross and dot products)
 
 # Let's introduce vector w
 g, h, i = sp.symbols('w1, w2, w3')
 w = sp.Matrix([g, h, i])
 
-lh = sp.simplify(u.cross(v.cross(w)))
-rh = sp.simplify((u.dot(w) * v) - (u.dot(v) * w))
-print('u x (v x w) = (u • w)v - (u • v)w: ', lh.equals(rh))
+lhs = sp.simplify(u.cross(v.cross(w)))
+rhs = sp.simplify((u.dot(w) * v) - (u.dot(v) * w))
+print('u x (v x w) = (u • w)v - (u • v)w: ', lhs.equals(rhs))
 
-# Prove the theorem (u x v) x w = (u • w)v - (v • w)u
+# Prove the theorem (u x v) x w = (u • w)v - (v • w)u (Relationship between cross and dot products)
 # Let's apply u x v = - (v x u) to the result of theorem u x (v x w) = (u • v)v - (u • v)w
 expression_A = u.cross(v).cross(w)
 
@@ -42,5 +42,17 @@ expressions = [expression_A, expression_B, expression_C, expression_D, expressio
 # If all expressions are equal to expression_A, it is proved that expression_A = expression_F
 all_equal = all(expression.equals(expressions[0]) for expression in expressions[1:])
 
-
 print('(u x v) x w = (u • w)v - (v • w)u: ', all_equal)
+
+# Prove Lagrange's identity ||u x v||^2 = ||u||^2 * ||v||^2 - (u • v)^2
+uv_cross = u.cross(v)
+left_hand_side = uv_cross[0]**2 + uv_cross[1]**2 + uv_cross[2]**2
+right_hand_side = (u[0]**2 + u[1]**2 + u[2]**2) * (v[0]**2 + v[1]**2 + v[2]**2) - u.dot(v)**2
+
+# Let's expand both expressions in order to visually demonstrate the equality
+print('left hand side: ', left_hand_side.expand())
+print('right hand side: ', right_hand_side.expand())
+
+result = left_hand_side.equals(right_hand_side)
+
+print('||u x v||^2 = ||u||^2 * ||v||^2 - (u • v)^2 :', result)
